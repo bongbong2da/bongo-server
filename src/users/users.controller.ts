0938f5707d8c7a19a0bda3313c39680d48a11e3a
bongbong2da <snk3981@gmail.com>
2024-08-22 19:03:10 +0900

    [Fix] Buggy commit fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
@@ -14,18 +14,20 @@ export class UsersController {
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
-  }
+    return this.usersService.findAll(":id"}
 
-  @Get(':":id"  findOne(@Param('i"id"id: string) {
+  @Get(':id')
+  "id"One(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':":id"  update(@Param('i"id"id: string, @Body() updateUserDto: UpdateUserDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @Delete(':":id"  remove(@Param('i"id"id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.usersService.remove(+id);
   }
 }
