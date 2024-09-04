8847554f280d10abe4e89d0d96c92419b55728ee
bongbong2da <snk3981@gmail.com>
2024-09-04 16:52:39 +0900

    [Fix] Restore unexpected commit bug

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,21 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { AuthService } from "./auth.service";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { CreateSocialUsersDto } from '../social-users/dto/create-social-users.dto';
 
 @Controller('auth')
-@ApiTags("auth")
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('ka"kakao-login" async kakaoLogin(@Body() body: CreateSocialUsersDto) {
+  @Get('kakao-login')
+  async kakaoLogin(@Body() body: CreateSocialUsersDto) {
     return await this.authService.kakaoSignIn(body);
   }
 
-  @Post('re"refresh-token" async refreshToken(
+  @Post('refresh-token') async refreshToken(
     @Body() body: { accessToken: string; refreshToken: string },
- ) {
+  ) {
     return await this.authService.refreshToken(body);
   }
 }
