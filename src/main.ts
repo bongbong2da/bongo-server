4c36990be9783298f2765384b75913e41bce56e6
bongbong2da <snk3981@gmail.com>
2024-09-14 00:00:54 +0900

    [Chore] Test console log

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3001);
+  await app.listen(3001).then(() => {
+    console.log('Server is running');
+  });
 }
 bootstrap();
