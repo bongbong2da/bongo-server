63cd0ac1434d0a9fd056dda231caeee3c993b124
bongbong2da <snk3981@gmail.com>
2024-09-20 23:41:36 +0900

    [Feat] Users API CRUD features

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,41 +1,135 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateUsersDto } from './dto/create-users.dto';
+import { UpdateUsersDto } from './dto/update-users.dto';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @ApiOperation({
+    operationId: 'createUser',
+    summary: '사용자 생성',
+    description: '사용자를 생성합니다.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '사용자 생성 성공',
+  })
+  @ApiBody({
+    type: CreateUsersDto,
+    required: true,
+    description: '사용자 정보',
+  })
+  create(@Body() createUserDto: CreateUsersDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @ApiOperation({
+    operationId: 'getUsers',
+    summary: '사용자 목록 조회',
+    description: '사용자 목록을 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '사용자 목록 조회 성공',
+  })
+  @ApiQuery({
+    name: 'page',
+    description: '페이지 번호',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'size',
+    description: '페이지 크기',
+    required: false,
+    type: Number,
+  })
+  findAll(
+    @Query('page', new DefaultValuePipe(0), ParseIntPipe) page: number,
+    @Query('size', new DefaultValuePipe(10), ParseIntPipe) size: number,
+  ) {
+    return this.usersService.findAll({ page, size });
   }
 
   @Get(':id')
+  @ApiOperation({
+    operationId: 'getUser',
+    summary: '사용자 조회',
+    description: '사용자를 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '사용자 조회',
+  })
+  @ApiQuery({
+    name: 'id',
+    description: '사용자 ID',
+    required: true,
+    type: Number,
+  })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  @ApiOperation({
+    operationId: 'updateUser',
+    summary: '사용자 수정',
+    description: '사용자를 수정합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '사용자 수정 성공',
+  })
+  @ApiQuery({
+    name: 'id',
+    description: '사용자 ID',
+    required: true,
+    type: Number,
+  })
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUsersDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    operationId: 'removeUser',
+    summary: '사용자 삭제',
+    description: '사용자를 삭제합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '사용자 삭제 성공',
+  })
+  @ApiQuery({
+    name: 'id',
+    description: '사용자 ID',
+    required: true,
+    type: Number,
+  })
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,89 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateUsersDto } from './dto/create-users.dto';
+import { makePagination } from '../utils/PaginationUtil';
+import { UpdateUsersDto } from './dto/update-users.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(user: CreateUsersDto) {
+    return this.prisma.users.create({
+      data: user,
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(params: { page: number; size: number }) {
+    const users = await this.prisma.users.findMany({
+      skip: params.page * params.size,
+      take: params.size,
+    });
+
+    const totalCount = await this.prisma.users.count();
+
+    const pagination = makePagination({
+      page: params.page,
+      size: params.size,
+      totalCount,
+    });
+
+    return {
+      data: users,
+      meta: pagination,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return this.prisma.users.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, user: UpdateUsersDto) {
+    const targetUser = await this.prisma.users.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!targetUser) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return this.prisma.users.update({
+      where: {
+        id,
+      },
+      data: user,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const targetUser = await this.prisma.users.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!targetUser) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    if (targetUser.id === 1) {
+      throw new UnauthorizedException('Only owner can delete the user');
+    }
+
+    return this.prisma.users.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
