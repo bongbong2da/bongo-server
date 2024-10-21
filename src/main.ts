e2801a947c3b95b23291ddd36174341250b35080
bongbong2da <snk3981@gmail.com>
2024-10-22 07:52:30 +0900

    [Modify] Enable swagger's bearer auth feature

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     .setTitle('Bongo API')
     .setDescription('Bongo application API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
