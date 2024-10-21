c130598e3e48255aa09959350736d93d527a4397
bongbong2da <snk3981@gmail.com>
2024-10-22 08:29:29 +0900

    [Modify] Enable notification service

--- src/bus-crews/bus-crews.module.ts
+++ src/bus-crews/bus-crews.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BusCrewsService } from './bus-crews.service';
 import { BusCrewsController } from './bus-crews.controller';
 import { PrismaService } from '../prisma.service';
+import { NotificationsService } from '../notifications/notifications.service';
 
 @Module({
   controllers: [BusCrewsController],
-  providers: [BusCrewsService, PrismaService],
+  providers: [BusCrewsService, PrismaService, NotificationsService],
 })
 export class BusCrewsModule {}
