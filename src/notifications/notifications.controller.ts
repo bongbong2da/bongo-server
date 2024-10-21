230fd2496b233e757308a4a58330b20036a866f2
bongbong2da <snk3981@gmail.com>
2024-10-22 08:14:33 +0900

    [Feat] Notifications features

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,7 +1,111 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import AuthGuard from '../guards/AuthGuard';
+import { Notifications } from './entities/notifications.entity';
 
 @Controller('notifications')
+@ApiTags('notifications')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Get()
+  @ApiOperation({
+    operationId: 'getNotifications',
+    summary: '알림 목록 조회',
+    description: '알림 목록을 조회합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: [Notifications],
+    description: '읽음 처리 성공',
+  })
+  @ApiQuery({
+    name: 'page',
+    description: '페이지 번호',
+    required: false,
+    type: Number,
+  })
+  @ApiQuery({
+    name: 'size',
+    description: '페이지 크기',
+    required: false,
+    type: Number,
+  })
+  getNotifications(
+    @Req() req,
+    @Query('page', new DefaultValuePipe(0)) page: number,
+    @Query('size', new DefaultValuePipe(10)) size: number,
+  ) {
+    const userId = req.user.sub;
+    return this.notificationsService.getNotifications(userId, { page, size });
+  }
+
+  @Post('read-all')
+  @ApiOperation({
+    operationId: 'readAllNotifications',
+    summary: '모든 알림 읽음 처리',
+    description: '모든 알림을 읽음 처리합니다.',
+  })
+  readAllNotifications(@Req() req) {
+    const userId = req.user.sub;
+    return this.notificationsService.readAllNotifications(userId);
+  }
+
+  @Post(':notificationId/read')
+  @ApiOperation({
+    operationId: 'readNotification',
+    summary: '알림 읽음 처리',
+    description: '알림을 읽음 처리합니다.',
+  })
+  @ApiQuery({
+    name: 'notificationId',
+    description: '알림 아이디',
+    required: true,
+    type: Number,
+  })
+  readNotification(
+    @Req() req,
+    @Query('notificationId', ParseIntPipe) notificationId: number,
+  ) {
+    const userId = req.user.sub;
+    return this.notificationsService.readNotification(userId, notificationId);
+  }
+
+  @Post(':notificationId/delete')
+  @ApiOperation({
+    operationId: 'deleteNotification',
+    summary: '알림 삭제',
+    description: '알림을 삭제합니다.',
+  })
+  @ApiQuery({
+    name: 'notificationId',
+    description: '알림 아이디',
+    required: true,
+    type: Number,
+  })
+  deleteNotification(
+    @Req() req,
+    @Query('notificationId', ParseIntPipe) notificationId: number,
+  ) {
+    const userId = req.user.sub;
+    return this.notificationsService.deleteNotification(userId, notificationId);
+  }
 }
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,26 +1,120 @@
-import { Injectable } from '@nestjs/common';
-import { CreateNotificationDto } from './dto/create-notification.dto';
-import { UpdateNotificationDto } from './dto/update-notification.dto';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { makePagination } from '../utils/PaginationUtil';
 
 @Injectable()
 export class NotificationsService {
-  create(createNotificationDto: CreateNotificationDto) {
-    return 'This action adds a new notification';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getNotifications(
+    userId: number,
+    params: { page: number; size: number },
+  ) {
+    try {
+      const notifications = await this.prisma.notifications.findMany({
+        where: {
+          userId,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        skip: params.page * params.size,
+        take: params.size,
+      });
+
+      const totalCount = await this.prisma.notifications.count({
+        where: {
+          userId,
+        },
+      });
+
+      const pagination = makePagination({
+        page: params.page,
+        size: params.size,
+        totalCount,
+      });
+
+      return {
+        notifications,
+        meta: pagination,
+      };
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 
-  findAll() {
-    return `This action returns all notifications`;
+  async readAllNotifications(userId: number) {
+    try {
+      await this.prisma.notifications.updateMany({
+        where: {
+          userId,
+          isRead: false,
+        },
+        data: {
+          isRead: true,
+        },
+      });
+
+      return true;
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} notification`;
+  async readNotification(userId: number, id: number) {
+    try {
+      const notification = await this.prisma.notifications.findFirst({
+        where: {
+          id,
+          userId,
+        },
+      });
+
+      if (!notification) {
+        throw new NotFoundException('Notification not found');
+      }
+
+      await this.prisma.notifications.update({
+        where: {
+          id,
+        },
+        data: {
+          isRead: true,
+        },
+      });
+
+      return true;
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 
-  update(id: number, updateNotificationDto: UpdateNotificationDto) {
-    return `This action updates a #${id} notification`;
-  }
+  async deleteNotification(userId: number, id: number) {
+    try {
+      const notification = await this.prisma.notifications.findFirst({
+        where: {
+          id,
+          userId,
+        },
+      });
 
-  remove(id: number) {
-    return `This action removes a #${id} notification`;
+      if (!notification) {
+        throw new NotFoundException('Notification not found');
+      }
+
+      await this.prisma.notifications.delete({
+        where: {
+          id,
+        },
+      });
+
+      return true;
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
   }
 }
